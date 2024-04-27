0f4ba44f3d887cfd49e01954ba47bc8131fde1a7
ayogatot <azerino25@gmail.com>
2024-04-27 19:02:39 +0700

    feat: add sharp for compressing image

--- src/utils/FileUpload.js
+++ src/utils/FileUpload.js
@@ -1,15 +1,15 @@
 import multer from "multer";
-import path from "path";
-import { v4 } from "uuid";
+// import path from "path";
+// import { v4 } from "uuid";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../uploads"));
-  },
-  filename: function (req, file, cb) {
-    const names = file.originalname.split(".");
-    cb(null, v4() + "." + names[names.length - 1]);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "../uploads"));
+//   },
+//   filename: function (req, file, cb) {
+//     const names = file.originalname.split(".");
+//     cb(null, v4() + "." + names[names.length - 1]);
+//   },
+// });
 
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage: multer.memoryStorage() });
