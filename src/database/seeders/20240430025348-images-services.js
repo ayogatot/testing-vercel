71616b3882cb977a26ad824461489560810e994a
ayogatot <azerino25@gmail.com>
2024-05-01 14:12:02 +0700

    feat: refactor some seeder, and add seeder for page services

---
+++ src/database/seeders/20240430025348-images-services.js
@@ -0,0 +1,145 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("images", [
+      {
+        image_id: "329f360f-1d94-4339-9ef2-0d0d6409a355",
+        page_id: "8e66c350-2137-469a-b9ee-ce06fa775a9d",
+        filename: "7701db78-821c-483b-9591-f738091251fc.png",
+        created_at: 1714537508368,
+        updated_at: null,
+      },
+      {
+        image_id: "41b2ca1c-30b5-444e-ae81-2ca0683efe7c",
+        page_id: "cdeff5ec-24da-43d1-99eb-ac1642a5b4ee",
+        filename: "f1eba334-278a-4a0a-a50d-7a326c748817.png",
+        created_at: 1714538874864,
+        updated_at: null,
+      },
+      {
+        image_id: "0b7f039c-3ce7-4dd1-8acd-9151debc6665",
+        page_id: "a2b727fa-cba7-470e-bcd6-009fda782e42",
+        filename: "493a24a8-ef25-4442-9f05-0bd36d555dd4.png",
+        created_at: 1714538939086,
+        updated_at: null,
+      },
+      {
+        image_id: "ddf226ce-7bb9-48da-aeb4-d6019bc56656",
+        page_id: "b22e8b45-273d-4869-90f7-ba056bc2be0e",
+        filename: "7a4b5924-cdcc-4dfc-b52c-523ba428fda6.png",
+        created_at: 1714538971179,
+        updated_at: null,
+      },
+      {
+        image_id: "f7fc41c9-bff0-4176-a0ec-3804c073c32b",
+        page_id: "ad88c1b8-f4de-414b-bd38-545b525dc862",
+        filename: "b896bb26-2d16-4237-ad19-84e420eb6981.png",
+        created_at: 1714539017848,
+        updated_at: null,
+      },
+      {
+        image_id: "ea21fd11-67f7-42b2-ade5-7c516c99db7a",
+        page_id: "afcd0b8d-7d9d-41cc-b0a0-3c6babf40497",
+        filename: "b147dd62-a219-4fc9-ae49-fdfd46030842.png",
+        created_at: 1714539046476,
+        updated_at: null,
+      },
+      {
+        image_id: "df49a528-515c-4845-b3d3-8a1f238fd921",
+        page_id: "ef6bd4b8-8d9e-4747-a887-ce308957ac84",
+        filename: "e6aa1a4d-d8bf-4e48-8084-0f4561e6fcc7.png",
+        created_at: 1714539083386,
+        updated_at: null,
+      },
+      {
+        image_id: "24ab37ac-3eec-4cc1-b382-a4d3b4acf277",
+        page_id: "33536cbd-742f-426a-ab36-670354495e94",
+        filename: "b988c638-cf1b-4bbb-bf72-581e09ae6d7c.png",
+        created_at: 1714540031240,
+        updated_at: null,
+      },
+      {
+        image_id: "b9670b03-6f50-453e-bfbe-aa996d3ce0e5",
+        page_id: "9979111b-e675-47ce-8346-426e32e3ab41",
+        filename: "1f6b5fb3-95ff-4e68-a8ab-fa5805d06dac.png",
+        created_at: 1714539457650,
+        updated_at: null,
+      },
+      {
+        image_id: "90c6bfdd-fc63-478e-83cc-f8cdf607aa8e",
+        page_id: "810dc098-46b2-471d-9d8e-57069ccaf881",
+        filename: "e6de27da-1c5b-4b2e-a9b4-e92f023457e5.png",
+        created_at: 1714539485296,
+        updated_at: null,
+      },
+      {
+        image_id: "bc8c765b-117d-4c97-b5bf-d64bf9704659",
+        page_id: "7eb6d969-16e2-46d2-b9d0-53ee314b9e0e",
+        filename: "c62a3c38-68df-4f0b-aa5e-9d28e443609e.png",
+        created_at: 1714539526193,
+        updated_at: null,
+      },
+      {
+        image_id: "3f67c6f5-03cf-4717-a2d0-ddd27d8f3f73",
+        page_id: "270d0454-3c5f-4375-b8a8-26d8410324a2",
+        filename: "b1d4d40c-4192-4b54-b7d9-de30b7f92a4e.png",
+        created_at: 1714539785962,
+        updated_at: null,
+      },
+      {
+        image_id: "1673221e-4ff6-4c5b-bb2c-fab4c620428e",
+        page_id: "6a8de880-6fb3-4525-8b41-d02e8a27485e",
+        filename: "75db2b91-0bad-4658-8eae-4dc4f2325c81.png",
+        created_at: 1714539865822,
+        updated_at: null,
+      },
+      {
+        image_id: "5fe03c5a-6107-4813-9326-3c92bc69fd1e",
+        page_id: "d15f2a39-1595-4d25-b3d9-7ed382b67469",
+        filename: "c2d2c247-fd07-4045-a613-1209ac358ebf.png",
+        created_at: 1714539887815,
+        updated_at: null,
+      },
+      {
+        image_id: "abe52db7-b5f8-4934-a6d6-6f8ee6d63abb",
+        page_id: "7fbd8ee6-ff48-46a5-8f6a-639ef1aea3a1",
+        filename: "9e7a5567-3dd4-448b-92e5-b752f6b94604.png",
+        created_at: 1714539903501,
+        updated_at: null,
+      },
+      {
+        image_id: "76a1d46a-f746-4a35-a57a-67b940873682",
+        page_id: "79d62267-79e8-49cb-83a1-6e802f91d444",
+        filename: "9ba3f10a-b01e-43a3-83c4-e962101adb6a.png",
+        created_at: 1714539915644,
+        updated_at: null,
+      },
+      {
+        image_id: "0d48427d-8a5d-41a0-beea-612e473801c0",
+        page_id: "a0c9a9c3-8d72-47f6-95f8-53854cddeef3",
+        filename: "00d77a2a-22fe-4b27-b83d-ecca584e88a2.png",
+        created_at: 1714539929123,
+        updated_at: null,
+      },
+      {
+        image_id: "42c27cc0-22ab-447c-94e5-da7f0435e1b0",
+        page_id: "251484fb-fdcb-480e-91d4-1a24e89f558a",
+        filename: "6d4c9764-6ec6-4dc3-a8e8-258592bc061a.png",
+        created_at: 1714539948215,
+        updated_at: null,
+      },
+      {
+        image_id: "0266975b-9374-463d-ac12-ffcb8b78ce32",
+        page_id: "20b3a1b5-9c1d-445d-83c4-f71738592a2c",
+        filename: "14bc4f0e-5b34-4fac-b268-d86d7e4301f6.png",
+        created_at: 1714541128200,
+        updated_at: null,
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("images", null, {});
+  },
+};
--- src/services/page.service.js
+++ src/services/page.service.js
@@ -16,16 +16,28 @@ pageService.add = async (page) => {
   logger().info(`add page, page = ${objectToLogStr(page)}`);
   const t = await connection.transaction();
   try {
+    page.created_at = new Date().getTime();
+
     const newPage = await Pages.create(page, { transaction: t });
     logger().info(`page created, page = ${newPage.page_id}`);
 
     if (page.images.length > 0) {
       const pageId = newPage.page_id;
       const images = await page.images.reduce(async (acc, curr) => {
-        const filename = v4() + '.png';
-        await sharp(curr.buffer).png({ quality: 70 }).toFile(path.join(__dirname, "../uploads", filename));
-        return [...(await acc), { ...curr, page_id: pageId, filename, created_at: new Date().getTime() }];
-      }, [])
+        const filename = v4() + ".png";
+        await sharp(curr.buffer)
+          .png({ quality: 70 })
+          .toFile(path.join(__dirname, "../uploads", filename));
+        return [
+          ...(await acc),
+          {
+            ...curr,
+            page_id: pageId,
+            filename,
+            created_at: new Date().getTime(),
+          },
+        ];
+      }, []);
       await Images.bulkCreate(images, { transaction: t });
       logger().info(`images created, for page_id = ${pageId}`);
     }
@@ -40,31 +52,33 @@ pageService.add = async (page) => {
 
 pageService.getAll = async (query) => {
   logger().info(`get all pages`);
-    const pages = await Pages.findAll({
-      where: getPagesQuery(query),
-      include: [
-        {
-          model: Images,
-          as: "images",
-          // attributes: ["image_id", "filename"],
-        },
-        {
-          model: Pages,
-          as: "childs",
-          required: false,
-          attributes: ["title", "subtitle", "description"],
-          include: [
-            {
-              model: Images,
-              as: "images",
-              // attributes: ["image_id", "filename"],
-            }
-          ]
-        }
-      ],
-    });
+  const pages = await Pages.findAll({
+    where: getPagesQuery(query),
+    include: [
+      {
+        model: Images,
+        as: "images",
+        attributes: ["image_id", "filename"],
+      },
+      {
+        model: Pages,
+        as: "childs",
+        required: false,
+        attributes: ["title", "subtitle", "description", "sequence"],
+        separate: true,
+        order: [["sequence", "ASC"]],
+        include: [
+          {
+            model: Images,
+            as: "images",
+            attributes: ["image_id", "filename"],
+          },
+        ],
+      },
+    ],
+  });
 
-    return pages;
-}
+  return pages;
+};
 
 export default pageService;
