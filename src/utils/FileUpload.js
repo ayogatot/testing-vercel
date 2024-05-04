7369ae1b500a333e7dddab3b58be302a611f856c
ayogatot <azerino25@gmail.com>
2024-05-04 12:59:06 +0700

    feat: modify file upload to handle separate file

--- src/utils/FileUpload.js
+++ src/utils/FileUpload.js
@@ -1,15 +1,16 @@
 import multer from "multer";
-// import path from "path";
-// import { v4 } from "uuid";
+import path from "path";
+import { v4 } from "uuid";
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, path.join(__dirname, "../uploads"));
-//   },
-//   filename: function (req, file, cb) {
-//     const names = file.originalname.split(".");
-//     cb(null, v4() + "." + names[names.length - 1]);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads/cv"));
+  },
+  filename: function (req, file, cb) {
+    const names = file.originalname.split(".");
+    cb(null, v4() + "." + names[names.length - 1]);
+  },
+});
 
 export const upload = multer({ storage: multer.memoryStorage() });
+export const uploadLocal = multer({ storage: storage });
