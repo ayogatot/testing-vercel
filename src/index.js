0650e92a4b8ca891139ec809ed4fe57d0725f18a
Arsyad Ramadhan <blacklistdark1987@gmail.com>
2024-09-28 13:49:19 +0700

    change method on index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
-import dotenv from "dotenv";
-import { logger } from "express-glass";
-import modules from "./modules";
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-(async () => {
-  try {
-    for (const module of modules) {
-      await module.init();
-    }
+import { logger } from 'express-glass'
+import sequelizeModule from './modules/sequelize.module'
 
-    logger().info("🚀 Server initialized successfully");
-  } catch (error) {
-    logger().error(error);
-    process.exit(1);
-  }
-})();
+import path from 'path'
+import cors from 'cors'
+import express from 'express'
+import compression from 'compression'
+import routes from './routes'
+import globalErrorHandler from './middlewares/errorHandler.middleware'
+
+const server = express()
+server.use(compression())
+server.use(express.json())
+server.use(cors({ origin: '*' }))
+server.use(express.urlencoded({ extended: true }))
+server.use('/uploads', express.static(path.join(__dirname, '../uploads')))
+
+// Base URL
+server.get('/', (req, res) => res.send('Hello World'))
+
+// Routes
+server.use('/api/v1/auth', routes.authRoutes)
+server.use('/api/v1/absences', routes.absencesRoutes)
+
+server.use(globalErrorHandler)
+
+// FOR VERCEL REQUIREMENTS
+server.listen(process.env.SERVER_PORT, async () => {
+	try {
+		await sequelizeModule.init()
+
+		logger().info('🚀 Server initialized successfully')
+	} catch (error) {
+		logger().error(error)
+		process.exit(1)
+	}
+})
+logger().info('[EXPRESS] Express initialized')
+
+module.exports = server
+
+// (async () => {
+//   try {
+//     for (const module of modules) {
+//       await module.init();
+//     }
+
+//     logger().info("🚀 Server initialized successfully");
+//   } catch (error) {
+//     logger().error(error);
+//     process.exit(1);
+//   }
+// })();
