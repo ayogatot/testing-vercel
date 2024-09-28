5b33da52465a80f3b9192ea42a198e153aeb5553
Arsyad Ramadhan <blacklistdark1987@gmail.com>
2024-09-28 11:24:40 +0700

    add module manually

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,20 @@
-require('dotenv/config');
+import mysql2 from 'mysql2'
+require('dotenv/config')
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true
-  },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-};
-
+	dialect: process.env.DB_DIALECT,
+	dialectModule: mysql2,
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	define: {
+		timestamps: true,
+	},
+	pool: {
+		max: 10,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+}
